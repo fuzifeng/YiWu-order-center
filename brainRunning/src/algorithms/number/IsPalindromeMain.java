f0b873002d8877524f862d77ce4cad94d29fe329
fuzifeng <fuzf@wdcloud.cc>
2021-02-26 18:58:44 +0800

    feat: suanfa

--- brainRunning/src/algorithms/number/IsPalindromeMain.java
+++ brainRunning/src/algorithms/number/IsPalindromeMain.java
@@ -35,15 +35,21 @@ package algorithms.number;
 public class IsPalindromeMain {
 
     public static void main(String[] args) {
-
+        System.out.println(isPalindrome(111121));
     }
 
     public static boolean isPalindrome(int x) {
-        if (x < 0) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
-
-
-        return false;
+        int y = 0;
+        while (x > y) {
+            y = 10 * y + x % 10;
+            x = x / 10;
+        }
+        // 当数字长度为奇数时，我们可以通过 revertedNumber/10 去除处于中位的数字。
+        // 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，revertedNumber = 123，
+        // 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
+        return x == y || x == y / 10;
     }
 }
---
+++ brainRunning/src/algorithms/number/ThreeNumsClosetMain.java
@@ -0,0 +1,57 @@
+package algorithms.number;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+ *
+ *  
+ *
+ * 示例：
+ *
+ * 输入：nums = [-1,2,1,-4], target = 1
+ * 输出：2
+ * 解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/3sum-closest
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author fuzf
+ */
+public class ThreeNumsClosetMain {
+
+    public static void main(String[] args) {
+        System.out.println(threeSumClosest(new int[]{-1,2,1,-4},1));
+    }
+
+
+    public static int threeSumClosest(int[] nums, int target) {
+        int closet = Integer.MAX_VALUE;
+        int result = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = nums[i];
+
+            int zhong = i + 1;
+            int right = nums.length - 1;
+
+            for (; zhong < nums.length - 1; zhong++) {
+                if (zhong != i + 1 && nums[zhong] == nums[zhong - 1]) {
+                    continue;
+                }
+                for (; right > zhong; right--) {
+                    if (nums[left] + nums[zhong] + nums[right] == target) {
+                        return target;
+                    }
+                    if (closet > Math.abs(target - (nums[left] + nums[zhong] + nums[right]))) {
+                        result = nums[left] + nums[zhong] + nums[right];
+                    }
+                }
+            }
+
+
+        }
+        return result;
+    }
+}
