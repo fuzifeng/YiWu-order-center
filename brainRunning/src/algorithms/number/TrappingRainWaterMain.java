35d8ee8432275d770fe9f8c117aab8e29a4d41bb
fuzifeng <fuzf@wdcloud.cc>
2021-03-02 11:21:11 +0800

    feat: some

---
+++ brainRunning/src/algorithms/number/TrappingRainWaterMain.java
@@ -0,0 +1,72 @@
+package algorithms.number;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ *
+ * 示例 1：
+ * 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出：6
+ * 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+ * 示例 2：
+ *
+ * 输入：height = [4,2,0,3,2,5]
+ * 输出：9
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/trapping-rain-water
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author fuzf
+ */
+public class TrappingRainWaterMain {
+
+    public static void main(String[] args) {
+        System.out.println(trap2(new int[]{4,2,0,3,2,5}));
+    }
+
+    public static int trap1(int[] height) {
+        int ans = 0, current = 0;
+        Deque<Integer> stack = new LinkedList<>();
+        while (current < height.length) {
+            while (!stack.isEmpty() && height[current] > height[stack.peek()]) {
+                int top = stack.pop();
+                if (stack.isEmpty()) {
+                    break;
+                }
+                int distance = current - stack.peek() - 1;
+                int bounded_height = Math.min(height[current],height[stack.peek()])-height[top];
+                ans += distance + bounded_height;
+            }
+            stack.push(current++);
+        }
+        return ans;
+    }
+
+    public static int trap2(int[] height) {
+        int ans = 0;
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    ans += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] < rightMax) {
+                    ans += rightMax - height[right];
+                } else {
+                    rightMax = height[right];
+                }
+                right--;
+            }
+        }
+        return ans;
+    }
+}
