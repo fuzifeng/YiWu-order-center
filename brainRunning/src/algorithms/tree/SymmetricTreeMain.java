70f309b4c8e4138a27a555d873f1e2d9461451df
fuzifeng <fuzf@wdcloud.cc>
2021-03-19 23:36:50 +0800

    feat: tree test

---
+++ brainRunning/src/algorithms/tree/SymmetricTreeMain.java
@@ -0,0 +1,15 @@
+package algorithms.tree;
+
+/**
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ *
+ * https://leetcode-cn.com/problems/symmetric-tree/
+ *
+ * @author fuzf
+ */
+public class SymmetricTreeMain {
+
+    public static boolean solution(TreeNode tree) {
+        return false;
+    }
+}
